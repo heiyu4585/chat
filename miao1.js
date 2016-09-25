805eb09f6ee5146ab8893620fe9c66bed6cceb74
黑雨 <wangning4567@163.com>
2016-09-25 21:44:44 +0800

    文件完毕

--- miao1.js
+++ miao1.js
@@ -1,34 +1,36 @@
 var Botton = React.createClass({
-  getInitialState: function () {
-    return {
-      opacity: 1
+    getInitialState: function () {
+        return {
+            opacity: 1
+        }
+    },
+
+    componentDidMount: function (eve) {
+        var opacity = this.state.opacity;
+        var _this = this;
+        this.timer = setInterval(function () {
+            opacity -= .05;
+            if (opacity < 0.1) {
+                opacity = 1
+            }
+            _this.setState({
+                opacity: opacity
+            })
+        }, 100)
+
+    },
+    render: function () {
+        var opacity = this.state.opacity;
+        console.log(opacity);
+        return (
+            <div style={{opacity:opacity}}>
+                hello {this.props.name}
+            </div>
+        )
     }
-  },
-
-  componentDidMount: function (eve) {
-    var opacity= this.state.opacity;
-    this.timer = setInterval(function(){
-      opacity-=.05;
-      if(opacity<0.1){
-        opacity =1
-      };
-      this.setState({
-        opacity:opacity
-      })
-    },100)
-
-  },
-  render: function () {
-
-    return (
-      <div style={{opacity:opacity}}>
-        hello {this.props.name}
-      </div>
-    )
-  }
 });
 
 ReactDOM.render(
-  <Botton name="world"/>,
-  document.getElementById("container")
+    <Botton name="world"/>,
+    document.getElementById("container")
 )
