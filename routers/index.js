dfe85a97d57af47d3ad15d7eacad69ef7022a198
黑雨 <wangning4567@163.com>
2016-10-17 23:25:14 +0800

    完成登陆

--- routers/index.js
+++ routers/index.js
@@ -17,48 +17,46 @@ db.open(function (err, db) {
             if (err) {
                 console.log(err);
             } else {
-
                 //登陆页面
                 router.get('/login', function (req, res, next) {
-                    //用户名和密码 时,查询数据库
-                    console.log(1);
-                    if (req.query.name && req.query.pass) {
-                        console.log(2);
-                        collection.find({name: req.query.name}).toArray(function (err, docs) {
-                            console.log(3);
-                            if (docs[0] && docs[0].password && (docs[0].password == req.query.pass)) {
 
+                    //用户名和密码 时,查询数据库
+                    if (req.query.name && req.query.password) {
+                        collection.find({name: req.query.name}).toArray(function (err, docs) {
+                            if (docs[0] && docs[0].password && (docs[0].password == req.query.password)) {
                                 console.log(5);
-                                res.redirect('/');
+                                res.redirect('/index');
+                                //res.redirect(404,'/');
+                                //res.location('/');
                             }
                         });
+
                     } else {
-                        console.log(4);
                         res.render('./index/login', {
                             title: '登录页'
                         });
                     }
+
                 });
 
-
+                //    // 用户列表
+                    router.get('/', function (req, res, next) {
+                    collection.find().toArray(function (err, docs) {
+                        //res.send('首页');
+                        res.render('index', {
+                            title: '首页-聊天室',
+                            authors: docs,
+                            author: req.query.name
+                        });
+                    });
+                });
             }
         });
     } else {
         console.log(err);
     }
 });
-// 用户列表
 
-router.get('/', function (req, res, next) {
-    collection.find().toArray(function (err, docs) {
-        //res.send('首页');
-        res.render('index', {
-            title: '首页-聊天室',
-            authors: docs,
-            author: req.query.name
-        });
-    });
-});
 
 
 //var User = require('../models/user');
